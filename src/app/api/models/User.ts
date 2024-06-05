f50f013e161c200ab093c2d580f3ad84ef60f753
wilber <parionahuaraca@gmail.com>
2024-06-05 13:20:06 -0500

    cambios

--- src/app/api/models/User.ts
+++ src/app/api/models/User.ts
@@ -1,5 +1,5 @@
 import { Schema, model, models } from "mongoose";
-import bcrypt from "bcryptjs/index";
+import bcrypt from "bcryptjs";
 
 const UserSchema = new Schema({
     email: { type: String, unique:true, required: [true, ' El correo es obligatorio ' ] }, 
